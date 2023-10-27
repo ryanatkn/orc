7366e29681c63cb40b291f9dbebaec7456f9ebfc
Ryan Atkinson <mail@ryanatkn.com>
2023-10-27 03:45:28 -0600

    wip

--- src/lib/fetch_packages.ts
+++ src/lib/fetch_packages.ts
@@ -9,7 +9,10 @@ import type {PackageMeta} from '@fuz.dev/fuz_library/package_meta.js';
 export interface FetchedPackage {
 	url: Url;
 	package_json: PackageJson | null; // TODO forward error
+	issues: GithubIssue[]; // TODO BLOCK type
 }
+
+type GithubIssue = any; // TODO BLOCK
 
 // TODO rethink with `PackageMeta`
 export type FetchedPackageMeta = PackageMeta | {url: Url; package_json: null};
@@ -24,20 +27,32 @@ export const fetch_packages = async (
 	console.log(`urls`, urls);
 	const packages: FetchedPackage[] = [];
 	for (const url of urls) {
-		const package_json_url = strip_end(url, '/') + '/.well-known/package.json'; // TODO helper
-		log?.info('fetching', package_json_url);
-		try {
-			const res = await fetch(package_json_url, {
-				headers: {'content-type': 'application/json', accept: 'application/json'},
-			});
-			const json = await res.json();
-			const package_json = PackageJson.parse(json); // TODO maybe not?
-			packages.push({url, package_json});
-		} catch (err) {
-			packages.push({url, package_json: null}); // TODO better error
-		}
+		const package_json = await load_package_json(url, log);
 		// TODO delay?
 		await wait(delay);
+		const issues = await fetch_github_issues(url);
+		if (!issues) throw Error('failed to fetch issues: ' + url);
+		await wait(delay);
+		packages.push({url, package_json, issues});
 	}
 	return packages;
+};
+
+const fetch_github_issues = async (url: string, log?: Logger): Promise<GithubIssue[] | null> => {
+	return null;
+};
+
+const load_package_json = async (url: string, log?: Logger): Promise<PackageJson | null> => {
+	const package_json_url = strip_end(url, '/') + '/.well-known/package.json'; // TODO helper
+	log?.info('fetching', package_json_url);
+	try {
+		const res = await fetch(package_json_url, {
+			headers: {'content-type': 'application/json', accept: 'application/json'},
+		});
+		const json = await res.json();
+		const package_json = PackageJson.parse(json); // TODO maybe not?
+		return package_json;
+	} catch (err) {
+		return null; // TODO better error
+	}
 };
