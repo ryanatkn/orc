36d53f82efb8673fbea2ebb56ed8859fb7391dcd
Ryan Atkinson <mail@ryanatkn.com>
2023-10-27 12:28:01 -0600

    wip

--- src/lib/sync.task.ts
+++ src/lib/sync.task.ts
@@ -5,7 +5,7 @@ import {cp} from 'fs/promises';
 // TODO refactor - upstream to gro? see also @feltjs/felt
 const init_env = async (): Promise<void> => {
 	const path = '.env';
-	const example_path = '.env.example';
+	const example_path = '.env.example'; // TODO check .production and .development too
 	if (!(await exists(path))) {
 		await cp(example_path, path);
 	}
