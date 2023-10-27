1ed8dc43079e43f5f3aa2eb024a9aa71aa6f07f8
Ryan Atkinson <mail@ryanatkn.com>
2023-10-27 02:33:50 -0600

    parse OrcConfig

---
+++ .changeset/blue-buckets-burn.md
@@ -0,0 +1,5 @@
+---
+'@ryanatkn/orc': patch
+---
+
+strict config
--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,11 +2,14 @@ import {paths, Url} from '@grogarden/gro/paths.js';
 import {join} from 'node:path';
 import {z} from 'zod';
 
-export const OrcConfig = z.object({
-	packages: z.array(Url),
-});
+export const OrcConfig = z
+	.object({
+		packages: z.array(Url),
+	})
+	.strict();
 export type OrcConfig = z.infer<typeof OrcConfig>;
 
-// TODO refactor for reusability
-export const load_orc_config = async (dir = paths.root): Promise<OrcConfig> =>
-	(await import(join(dir, 'orc.config.ts'))).default;
+export const load_orc_config = async (dir = paths.root): Promise<OrcConfig> => {
+	const config = (await import(join(dir, 'orc.config.ts'))).default;
+	return OrcConfig.parse(config);
+};
