ba45f6c035b5db3bae42612a2420f77e80542817
Ryan Atkinson <mail@ryanatkn.com>
2023-10-27 12:27:14 -0600

    wip

---
+++ src/lib/sync.task.ts
@@ -0,0 +1,20 @@
+import {exists} from '@grogarden/gro/exists.js';
+import {task as base_task} from '@grogarden/gro/sync.task.js';
+import {cp} from 'fs/promises';
+
+// TODO refactor - upstream to gro? see also @feltjs/felt
+const init_env = async (): Promise<void> => {
+	const path = '.env';
+	const example_path = '.env.example';
+	if (!(await exists(path))) {
+		await cp(example_path, path);
+	}
+};
+
+export const task: typeof base_task = {
+	...base_task,
+	run: async ({invoke_task, args}) => {
+		await init_env();
+		await invoke_task('gro/sync', args);
+	},
+};
