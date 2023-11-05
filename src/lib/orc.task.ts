d676478cee4268cbd3ea1dd9433d6e1f4727591e
Ryan Atkinson <mail@ryanatkn.com>
2023-11-05 00:00:27 -0600

    todo

--- src/lib/orc.task.ts
+++ src/lib/orc.task.ts
@@ -4,14 +4,15 @@ import {z} from 'zod';
 export const Args = z.object({}).strict();
 export type Args = z.infer<typeof Args>;
 
-/**
- * This is a task not a `.gen.` file because it makes network calls.
- */
 export const task: Task<Args> = {
 	Args,
 	summary: 'upgrade and update packages',
 	run: async ({invoke_task}) => {
 		await invoke_task('upgrade');
 		await invoke_task('packages');
+		await invoke_task('sync');
+		// TODO probably support an optional message,
+		// and then `gro changeset` (if there are any!)
+		// and then `git commit` and `git push`
 	},
 };
