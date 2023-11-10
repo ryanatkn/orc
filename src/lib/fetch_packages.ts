c2dfa09a8538e5ccaa561b53296b341da5838720
Ryan Atkinson <mail@ryanatkn.com>
2023-11-09 18:52:30 -0700

    wip

--- src/lib/fetch_packages.ts
+++ src/lib/fetch_packages.ts
@@ -75,7 +75,7 @@ export const fetch_packages = async (
 	return packages;
 };
 
-// TODO BLOCK make this work with other urls and text
+// TODO BLOCK make this work with other urls and text, and extract
 // TODO refactor with `fetch_github_pull_requests`
 const fetch_package_json = async (
 	homepage_url: string,
