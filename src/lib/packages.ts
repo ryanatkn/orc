0a7f5373c369a6448047ed8ff6e3b08a437452c1
Ryan Atkinson <mail@ryanatkn.com>
2023-11-10 19:39:51 -0700

    wip

--- src/lib/packages.ts
+++ src/lib/packages.ts
@@ -10,8 +10,8 @@ import type {
 // TODO BLOCK what relationship does this have to `Deployments`?
 // TODO BLOCK name? move to `fetch_packages`?
 export interface Packages {
-	pkgs: Fetched_Package[];
 	pkg: Fetched_Package;
+	pkgs: Fetched_Package[];
 	unfetched_pkgs: Unfetched_Package[];
 }
 
@@ -22,23 +22,18 @@ export const set_packages = (packages: Packages): Packages => setContext(KEY, pa
 export const get_packages = (): Packages => getContext(KEY);
 
 export const parse_packages = (packages: Maybe_Fetched_Package[]): Packages => {
-	const pkgs: Fetched_Package[] = packages
-		.map(({url, package_json, src_json, pull_requests}) =>
-			package_json && src_json
-				? {...parse_package_meta(url, package_json, src_json), pull_requests}
-				: null!,
-		)
-		.filter(Boolean);
+	const pkgs: Fetched_Package[] = [];
+	const unfetched_pkgs: Unfetched_Package[] = [];
+
+	for (const {url, package_json, src_json, pull_requests} of packages) {
+		if (package_json && src_json) {
+			pkgs.push({...parse_package_meta(url, package_json, src_json), pull_requests});
+		} else {
+			unfetched_pkgs.push({url, package_json: null, src_json: null, pull_requests: null});
+		}
+	}
 
 	const pkg = pkgs[0];
 
-	const unfetched_pkgs: Unfetched_Package[] = packages
-		.map(({url, package_json, src_json}) =>
-			package_json && src_json
-				? null!
-				: {url, package_json: null, src_json: null, pull_requests: null},
-		)
-		.filter(Boolean);
-
-	return {pkgs, pkg, unfetched_pkgs};
+	return {pkg, pkgs, unfetched_pkgs};
 };
