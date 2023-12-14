afc2108986b027de07f77b39f998f03aa9911e96
Ryan Atkinson <mail@ryanatkn.com>
2023-12-13 19:13:44 -0700

    wip

---
+++ src/lib/Deployments_Tree_Nav.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import {base} from '$app/paths';
+
+	import type {Fetched_Deployment} from '$lib/fetch_deployments.js';
+
+	export let deployments: Fetched_Deployment[];
+
+	/**
+	 * The selected package, if any.
+	 */
+	export let selected_deployment: Fetched_Deployment | undefined = undefined;
+</script>
+
+<nav>
+	<menu class="names panel padded_md">
+		{#each deployments as deployment}
+			{@const active = deployment === selected_deployment}
+			<li style:display="contents">
+				{#if deployment.package_json}<a
+						class="menu_item"
+						class:active
+						href="{base}/tree/{deployment.repo_name}"
+						><div class="ellipsis">
+							{deployment.repo_name}{#if deployment.package_json.icon}{' '}{deployment.package_json
+									.icon}{/if}
+						</div></a
+					>{/if}
+			</li>
+		{/each}
+	</menu>
+	<slot />
+</nav>
+
+<style>
+	nav {
+		width: var(--nav_width, 240px);
+		position: sticky;
+		top: var(--spacing_md);
+		padding: var(--spacing_lg);
+		padding-right: 0;
+	}
+	.names {
+		padding: var(--spacing_md);
+	}
+	/* TODO should be a CSS class or variable, probably add :focus behavior */
+	.names a:hover {
+		background-color: var(--bg_5);
+	}
+	.names a:is(:active, .active) {
+		background-color: var(--bg_7);
+	}
+</style>
