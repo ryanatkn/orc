f2d3050dee85ec77540806bc321090616ee3a48d
Ryan Atkinson <mail@ryanatkn.com>
2023-12-17 16:41:24 -0700

    upgrade @grogarden/util and switch to use its `fetch_value` (#18)

---
+++ .changeset/lazy-drinks-study.md
@@ -0,0 +1,5 @@
+---
+'@ryanatkn/orc': minor
+---
+
+upgrade @grogarden/util and switch to use its `fetch_value`
--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,16 +1,11 @@
 import type {Logger} from '@grogarden/util/log.js';
 import type {Package_Meta} from '@fuz.dev/fuz_library/package_meta.js';
-import {request} from '@octokit/request';
 import {z} from 'zod';
+import {fetch_value, type Fetch_Value_Cache} from '@grogarden/util/fetch.js';
 
-import {
-	to_fetch_cache_key,
-	type Fetch_Cache_Data,
-	type Fetch_Cache_Item,
-} from '$lib/fetch_cache.js';
-
-export const GITHUB_API_VERSION = '2022-11-28';
-
+/**
+ * @see https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
+ */
 export const Github_Pull_Request = z.object({
 	number: z.number(),
 	title: z.string(),
@@ -20,22 +15,78 @@ export const Github_Pull_Request = z.object({
 	draft: z.boolean(),
 });
 export type Github_Pull_Request = z.infer<typeof Github_Pull_Request>;
+export const Github_Pull_Requests = z.array(Github_Pull_Request);
+export type Github_Pull_Requests = z.infer<typeof Github_Pull_Requests>;
 
 /**
- * @see https://docs.github.com/en/rest/checks/runs#list-check-runs-for-a-git-reference
+ * @see https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
  */
-export const Github_Check_Runs = z.object({
+export const fetch_github_pull_requests = async (
+	pkg: Package_Meta,
+	cache?: Fetch_Value_Cache,
+	log?: Logger,
+	token?: string,
+	api_version?: string,
+): Promise<Github_Pull_Requests | null> => {
+	if (!pkg.owner_name) throw Error('owner_name is required');
+	const headers = api_version ? new Headers({'x-github-api-version': api_version}) : undefined;
+	const url = `https://api.github.com/repos/${pkg.owner_name}/${pkg.repo_name}/pulls`;
+	const fetched = await fetch_value(url, {
+		request: {headers},
+		parse: Github_Pull_Requests.parse,
+		token,
+		cache,
+		log,
+	});
+	if (!fetched.ok) return null;
+	return fetched.value;
+};
+
+/**
+ * @see https://docs.github.com/en/rest/checks/runs?apiVersion=2022-11-28#list-check-runs-for-a-git-reference
+ */
+export const Github_Check_Runs_Item = z.object({
 	status: z.enum(['queued', 'in_progress', 'completed']),
 	conclusion: z
 		.enum(['success', 'failure', 'neutral', 'cancelled', 'skipped', 'timed_out', 'action_required'])
 		.nullable(),
 });
+export type Github_Check_Runs_Item = z.infer<typeof Github_Check_Runs_Item>;
+export const Github_Check_Runs = z.object({
+	total_count: z.number(),
+	check_runs: z.array(Github_Check_Runs_Item),
+});
 export type Github_Check_Runs = z.infer<typeof Github_Check_Runs>;
 
-const reduce_check_runs = (check_runs: Github_Check_Runs[]): Github_Check_Runs | null => {
+/**
+ * @see https://docs.github.com/en/rest/checks/runs?apiVersion=2022-11-28#list-check-runs-for-a-git-reference
+ */
+export const fetch_github_check_runs = async (
+	pkg: Package_Meta,
+	cache?: Fetch_Value_Cache,
+	log?: Logger,
+	token?: string,
+	api_version?: string,
+	ref = 'main',
+): Promise<Github_Check_Runs_Item | null> => {
+	if (!pkg.owner_name) throw Error('owner_name is required');
+	const headers = api_version ? new Headers({'x-github-api-version': api_version}) : undefined;
+	const url = `https://api.github.com/repos/${pkg.owner_name}/${pkg.repo_name}/commits/${ref}/check-runs`;
+	const fetched = await fetch_value(url, {
+		request: {headers},
+		parse: (v) => reduce_check_runs(Github_Check_Runs.parse(v).check_runs),
+		token,
+		cache,
+		log,
+	});
+	if (!fetched.ok) return null;
+	return fetched.value;
+};
+
+const reduce_check_runs = (check_runs: Github_Check_Runs_Item[]): Github_Check_Runs_Item | null => {
 	if (!check_runs.length) return null;
-	let status!: Github_Check_Runs['status'];
-	let conclusion!: Github_Check_Runs['conclusion'];
+	let status!: Github_Check_Runs_Item['status'];
+	let conclusion!: Github_Check_Runs_Item['conclusion'];
 	for (const check_run of check_runs) {
 		if (!status || status === 'completed') {
 			status = check_run.status;
@@ -45,126 +96,4 @@ const reduce_check_runs = (check_runs: Github_Check_Runs[]): Github_Check_Runs |
 		}
 	}
 	return {status, conclusion};
-};
-
-// TODO refactor with `fetch_package_json` and `fetch_github_check_runs`
-/**
- * @see https://docs.github.com/en/rest/pulls/pulls#list-pull-requests
- */
-export const fetch_github_pull_requests = async (
-	url: string,
-	pkg: Package_Meta,
-	cache?: Fetch_Cache_Data,
-	log?: Logger,
-	token?: string,
-	api_version = GITHUB_API_VERSION,
-): Promise<Fetch_Cache_Item<Github_Pull_Request[] | null>> => {
-	log?.info('url', url);
-	if (!pkg.owner_name) throw Error('owner_name is required');
-	const params = {owner: pkg.owner_name, repo: pkg.repo_name} as const; // defaults to `sort: 'created'`
-	const headers: Record<string, string> = {
-		accept: 'application/vnd.github+json', // might be set by the library, @see https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
-		'x-github-api-version': api_version,
-	};
-	if (token) headers.authorization = 'Bearer ' + token;
-	const route = 'GET /repos/{owner}/{repo}/pulls'; // TODO param
-	const key = to_fetch_cache_key(route, params);
-	const cached = cache?.get(key);
-	const etag = cached?.etag;
-	if (etag) {
-		headers['if-none-match'] = etag;
-	}
-	try {
-		const res = await request(route, {
-			headers,
-			...params,
-		});
-		log?.info('not cached', key);
-		log?.info('res.headers', res.headers);
-		const result: Fetch_Cache_Item<Github_Pull_Request[] | null> = {
-			url,
-			params,
-			key,
-			etag: res.headers.etag ?? null,
-			last_modified: res.headers['last-modified'] ?? null,
-			data: res.data.map((i) => Github_Pull_Request.parse(i)).sort((a, b) => b.number - a.number),
-		};
-		cache?.set(result.key, result);
-		return result;
-	} catch (err) {
-		// TODO better error handling?
-		if (err.status === 304) {
-			log?.info('cached', key);
-			return cached!;
-		}
-		const result: Fetch_Cache_Item<Github_Pull_Request[] | null> = {
-			url,
-			params,
-			key,
-			etag: null,
-			last_modified: null,
-			data: null,
-		};
-		return result;
-	}
-};
-
-// TODO refactor with `fetch_package_json` and `fetch_github_pull_requests`
-/**
- * @see https://docs.github.com/en/rest/pulls/pulls#list-pull-requests
- */
-export const fetch_github_check_runs = async (
-	url: string,
-	pkg: Package_Meta,
-	cache?: Fetch_Cache_Data,
-	log?: Logger,
-	token?: string,
-	api_version = GITHUB_API_VERSION,
-	ref = 'main',
-): Promise<Fetch_Cache_Item<Github_Check_Runs | null>> => {
-	log?.info('url', url);
-	if (!pkg.owner_name) throw Error('owner_name is required');
-	const params = {owner: pkg.owner_name, repo: pkg.repo_name, ref} as const;
-	const headers: Record<string, string> = {
-		accept: 'application/vnd.github+json', // might be set by the library, @see https://docs.github.com/en/rest/pulls/pulls?apiVersion=2022-11-28#list-pull-requests
-		'x-github-api-version': api_version,
-	};
-	if (token) headers.authorization = 'Bearer ' + token;
-	const route = 'GET /repos/{owner}/{repo}/commits/{ref}/check-runs'; // TODO param
-	const key = to_fetch_cache_key(route, params);
-	const cached = cache?.get(key);
-	const etag = cached?.etag;
-	if (etag) headers['if-none-match'] = etag;
-	try {
-		const res = await request(route, {
-			headers,
-			...params,
-		});
-		log?.info('res.headers', key, res.headers);
-		const result: Fetch_Cache_Item<Github_Check_Runs | null> = {
-			url,
-			params,
-			key,
-			etag: res.headers.etag ?? null,
-			last_modified: res.headers['last-modified'] ?? null,
-			data: reduce_check_runs(res.data.check_runs.map((i) => Github_Check_Runs.parse(i))),
-		};
-		cache?.set(result.key, result);
-		return result;
-	} catch (err) {
-		// TODO better error handling?
-		if (err.status === 304) {
-			log?.info('cached', key);
-			return cached!;
-		}
-		const result: Fetch_Cache_Item<Github_Check_Runs | null> = {
-			url,
-			params,
-			key,
-			etag: null,
-			last_modified: null,
-			data: null,
-		};
-		return result;
-	}
 };
